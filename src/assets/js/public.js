4ad68a2547f2ae7f1e10f0c7c08b12279a99e833
Furqan Khanzada <furqan.khanzada@gmail.com>
2015-06-23 12:44:10 +0500

    implemented columns

--- src/assets/js/public.js
+++ src/assets/js/public.js
@@ -31,7 +31,24 @@
         $.when(getTemplate, getPosts).done(function(template, posts){
             // the code here will be executed when all 2 ajax requests resolve.
             // status, and jqXHR object for each of the 2 ajax calls respectively.
-            var result = _.template(template[0], {categories: posts[0]});
+            //var columns = posts[0]
+            //Making columns
+            var count = 1;
+            var index = 0;
+            var data = [];
+            _.each(posts[0], function(cat, key){
+                var obj = {};
+                obj[key] = cat;
+                data[index] ? data[index].push(obj) : (data[index] = [obj]);
+                //move to next after adding 2 categories to each column
+                if(count % 2 == 0){
+                    index++;
+                }
+                count++
+            });
+
+            console.log('data :', data);
+            var result = _.template(template[0], {columns: data});
             $container.append(result);
 
             console.log(posts[0])
